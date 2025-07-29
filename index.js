1b5f657b51d7d6420e2c9db5e5b7adfa29e42352
km-saifullah <kmsaifullah16@gmail.com>
2025-07-29 23:11:17 +0600

    add test route

--- index.js
+++ index.js
@@ -6,20 +6,8 @@ app.get("/", (req, res) => {
   res.end("<h1>Hello World</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.end("<h1>ABout Page</h1>");
-});
-
-app.get("/me", (req, res) => {
-  res.end("<h1>Hi everyone</h1>");
-});
-
-app.get("/monmoy", (req, res) => {
-  res.end("<h1>Hi Monmoy</h1>");
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<h1>Hello folks</h1>");
+app.get("/test", (req, res) => {
+  res.end("<h1>Hello universe</h1>");
 });
 
 app.listen(8000, () => console.log("Server is running"));
