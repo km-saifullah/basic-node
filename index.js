5a3e15008ac683d0ac895cd510d93998580d1ff1
km-saifullah <kmsaifullah16@gmail.com>
2025-07-29 22:43:30 +0600

    added one new route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
   res.end("<h1>Hello World</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.end("<h1>ABout Page</h1>");
+});
+
 app.listen(8000, () => console.log("Server is running"));
