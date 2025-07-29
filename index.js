dcb0a935996c343bf0168f44d5ad051a43749778
km-saifullah <kmsaifullah16@gmail.com>
2025-07-29 22:18:12 +0600

    simple node app done

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("<h1>Hello World</h1>");
+});
+
+app.listen(8000, () => console.log("Server is running"));
